bc326c91e85596ccdf23f058fbc62b2fe2a3cadd
Tekie79 <76453162+Tekie79@users.noreply.github.com>
2026-09-21 10:31:36 -0400

    Export speaker marker service

--- meher_resolve_hub/services/__init__.py
+++ meher_resolve_hub/services/__init__.py
@@ -4,7 +4,14 @@ from .health_service import HealthService
 from .marker_service import MarkerService
 from .metadata_service import MetadataService
 from .rename_service import RenameService
+from .speaker_marker_service import SpeakerMarkerService
 from .still_service import StillService
 
-__all__ = ["HealthService", "MarkerService", "MetadataService", "RenameService", "StillService"]
-
+__all__ = [
+    "HealthService",
+    "MarkerService",
+    "MetadataService",
+    "RenameService",
+    "SpeakerMarkerService",
+    "StillService",
+]
